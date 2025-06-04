bb5790615bbde33757e4d1fd6985aa72b0cc9ed6
chethan-gen <chaithuregala123@gmailcom>
2025-06-04 09:47:17 +0530

    authentication

--- client/src/features/auth/Register.jsx
+++
@@ -1,3 +0,0 @@
-export default function Register() {
-  return <div className="text-center text-3xl mt-10">📝 Register Page</div>;
-}
--- client/src/features/dashboard/Dashboard.jsx
+++
@@ -1,3 +0,0 @@
-export default function Dashboard() {
-  return <div className="text-center text-3xl mt-10">📊 User Dashboard</div>;
-}
